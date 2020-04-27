80a617e37c9e395e8ed4642e41f375a4bf810507
yyl7727 <772788236@qq.com>
2020-04-27 12:08:40 +0800

    first

---
+++ src/main/java/com/yyl/lysithea/LysitheaApplication.java
@@ -0,0 +1,13 @@
+package com.yyl.lysithea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LysitheaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LysitheaApplication.class, args);
+    }
+
+}
